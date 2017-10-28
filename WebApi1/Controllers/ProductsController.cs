d5f6732f34add98722c021019c245315e73e91ac
shravan567 <shravankumar567@gmail.com>
2017-10-28 10:42:09 -0400

    Intial Commit

---
+++ WebApi1/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi1.Models;
+
+namespace WebApi1.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Products[] products = new Products[]
+        {
+            new Products { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Products { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Products { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        };
+
+        public IEnumerable<Products> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ WebApi1/Models/Products.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi1.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
